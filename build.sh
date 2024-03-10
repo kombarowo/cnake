94a1df0f2ac62fc24cdfec123623c92ee93c1860
Artyom Kombarov <artyom.kombaroff@gmail.com>
2024-03-10 14:25:32 +0300

    Remove clear from build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
 
-clear; gcc -Wall -Werror src/main.c src/snake.c src/food.c src/terminal.c src/logic.c src/render.c -o build/main
+gcc -Wall -Werror src/main.c src/snake.c src/food.c src/terminal.c src/logic.c src/render.c -o build/main
